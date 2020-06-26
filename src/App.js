4599a3061db9a19a3abfe3808b1a9a926f5294a0
Anderson Viana <andiana@hotmnai.com>
2020-06-26 18:10:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import Users from './components/Users/Users';
+import Toggle from './components/Toggle/Toggle';
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [showUsers, setShowUsers] = useState(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch(
+        'https://randomuser.me/api/?seed=rush&nat=br&results=10'
+      );
+      const json = await res.json();
+      setUsers(json.results);
+    };
+    fetchUsers();
+  }, []);
+
+  const handleShowUsers = (isChecked) => {
+    setShowUsers(isChecked);
+  };
+
+  return (
+    <div>
+      <h3>React Lifecycle</h3>
+      <Toggle
+        description={'Mostrar Usuários'}
+        enabled={showUsers}
+        onToggle={handleShowUsers}
+      />
+      <hr />
+      {showUsers && <Users users={users} />}
+    </div>
+  );
+}
